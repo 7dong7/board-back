16e449d4d2d21d1634763d2222de7a460de3757e
7dong7 <tmdkdl777@gmail.com>
2024-12-06 00:31:25 +0900

    assert test

--- src/test/java/mystudy/study/repository/MemberRepositoryTest.java
+++ src/test/java/mystudy/study/repository/MemberRepositoryTest.java
@@ -9,8 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,11 +37,11 @@ class MemberRepositoryTest {
         memberRepository.save(member3);
         memberRepository.save(member4);
     }
-    
+
     // 기본 테스트
     @Test
     public void basicTest() throws Exception{
-        
+
         // 컬렉션 전체 조회
         List<Member> members = memberRepository.findAll();
 
@@ -52,12 +51,12 @@ class MemberRepositoryTest {
 
         assertThat(members.size()).isEqualTo(4);
     }
-    
+
     // optional 테스트
     @Test
     public void optionalTest() throws Exception{
 
-        // 이메일로 사용자 찾기 
+        // 이메일로 사용자 찾기
             // 이메일에 해당하는 사용자가 있을수도 없을수도 있음
         Optional<Member> findMember = memberRepository.findByUsername("member1");
         Optional<Member> findMember2 = memberRepository.findByUsername("memb");
@@ -71,5 +70,84 @@ class MemberRepositoryTest {
 //        System.out.println("nonMember = " + nonMember);
     }
 
+    // assert test
+    @Test
+    public void assertTest() throws Exception{
+
+        Optional<Member> findMemberOption = memberRepository.findByUsername("member1");
+
+        Member member = findMemberOption.orElseThrow(() -> new IllegalArgumentException("사용자가 없음"));
+
+        System.out.println("member = " + member);
+
+        assertThat(member.getUsername()).isEqualTo("member1");
+//        assertThat(member.getUsername()).as("찾는 사용자가 아닙니다.").isEqualTo("member2");
+    }
+
+
+    @Test
+    public void assertTest2() throws Exception{
+
+        String str = "hello, world, AssertJ";
+
+        System.out.println("str = " + str);
+
+        assertThat(str).isNotNull()
+                .startsWith("he")   // he로 시작하는지 확인
+                .contains("wor")    // wor 이 포함되어 있는지
+                .endsWith("tJ")     // tJ 로 끝나는지
+                .hasSize(21);   // 길이가 21 인지
+
+    }
+
+    // list 검증
+    @Test
+    void listTest() {
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+
+        assertThat(names)           // names 리스트를 대상으로
+                .isNotEmpty()       // 리스트가 비어있는지
+                .hasSize(3)     // 리스트 사이즈가 3인지
+                .contains("Alice", "Bob")   // 리스트에 Alice, Bob
+                .doesNotContain("David")    // David 가 포함되어 있지 있으면 에러
+                .containsExactly("Alice", "Bob", "Charlie") // 리스트가 정확히 지정된 요소들로 구성되어 있는지 (순서도 맞아야 됨)
+                .containsExactlyInAnyOrder("Charlie", "Bob", "Alice") // 리스타 정확히 지정된 요소들로 구성되어 있는지 (순서 상관 X)
+                .startsWith("Alice")    // 콜렉션 Alice 로 시작하는지
+                .endsWith("Charlie"); // 콜렉션 Charlie 로 끝나는지
+    }
+    
+    // list 중첩 검증
+    @Test
+    void nestedListTest() {
+        List<List<String>> nestedList = Arrays.asList(
+                Arrays.asList("A1", "A2"),
+                Arrays.asList("B1", "B2"),
+                Arrays.asList("C1", "C2")
+        );
+
+//        assertThat(nestedList)
+//                .hasSize(3)
+//                .extracting(List::size)
+//                .containsOnly(2, 2, 2)
+//                .flatExtracting(list -> list)
+//                .containsExactly("A1", "A2", "B1", "B2", "C1", "C2");
+    }
+
+    // map 검증
+    @Test
+    void mapTest() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Alice", 30);
+        map.put("Bob", 25);
+
+        assertThat(map)
+                .isNotEmpty()
+                .hasSize(2)
+                .containsKeys("Alice", "Bob")
+                .containsValues(25, 30)
+                .containsEntry("Alice", 30)
+                .doesNotContainKey("Charlie")
+                .doesNotContainValue(40);
+    }
 
 }
