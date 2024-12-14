309a6c9f400065d988c28cf177c898723dfb2740
7dong7 <tmdkdl777@gmail.com>
2024-12-14 13:39:40 +0900

    메인 소스 샘플 데이터 추가 테스트, 메인 데이터 분리

---
+++ src/main/java/mystudy/study/controller/init/InitMember.java
@@ -0,0 +1,52 @@
+package mystudy.study.controller.init;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import mystudy.study.domain.entity.Member;
+import mystudy.study.domain.entity.Post;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Profile("local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    @Component
+    static class InitMemberService {
+
+        @PersistenceContext
+        EntityManager em;
+
+        @Transactional
+        public void init() {
+            Member member1 = new Member("member1", 10, "member1@naver.com");
+            Member member2 = new Member("member2", 20, "member2@naver.com");
+
+            for ( int i = 0; i < 10; i++ ) {
+                Post post = new Post("새로운 글작성"+i, "새로운 글이 작성되었습니다"+i, member1);
+                member1.addPost(post);
+            }
+
+            for ( int i = 0; i < 5; i++ ) {
+                Post post = new Post("새로운 글작성"+i, "새로운 글이 작성되었습니다"+i, member2);
+                member2.addPost(post);
+            }
+
+            em.persist(member1);
+            em.persist(member2);
+        }
+    }
+
+}
